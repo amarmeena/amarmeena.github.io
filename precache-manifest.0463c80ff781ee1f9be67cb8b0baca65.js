f802fc3ae3340f5fffeb89689dab2c75c6297244
Amar Meena <amarmeena1992@gmail.com>
2020-06-02 19:32:42 -0400

    Updates

---
+++ precache-manifest.0463c80ff781ee1f9be67cb8b0baca65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3ac60d53fce6c01c10158c03aa9de8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "97a1ab934b149c8bc254",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "015487fa8f98a7d06117",
+    "url": "/portfolio/static/js/2.60e1175f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.60e1175f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a1ab934b149c8bc254",
+    "url": "/portfolio/static/js/main.7bb0fff0.chunk.js"
+  },
+  {
+    "revision": "223e5ba1997dc15361b6",
+    "url": "/portfolio/static/js/runtime-main.ec175145.js"
+  }
+]);
