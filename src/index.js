50e2a7bc679873b17819acbe8292faf7f7fe0eb0
Amar Meena <amarmeena1992@gmail.com>
2020-06-03 14:15:07 -0400

    change root directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App />
-</Router>, document.getElementById('root'));
-registerServiceWorker();
